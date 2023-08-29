f1ed7785a2833360bb878f9e18b10de64957ca51
ssok305 <sutherysok@gmail.com>
2023-08-29 11:45:02 -0600

    Finished API calls to get data

--- backend/controllers/apiController.js
+++ backend/controllers/apiController.js
@@ -1,8 +1,80 @@
-function apiController () {
-    const clientID = " "
-    const clientSecret = " "
+const { default: axios } = require("axios")
 
-    const getToken = async() = {
-        
-    }
+
+async function getToken(){
+    const response = await axios(`https://api.spotify.com/api/token`, {
+        method: 'POST',
+        headers: {
+            'Content-Type' : 'application/x-www-form-urlencoded',
+            'Authorization' : `Bearer` + btoa(process.env.CLIENT_ID + ':' + process.env.CLIENT_SECRET)
+        },
+        body: 'grant_type=client_credentials'
+    })
+
+    const data = await response.json()
+    console.log(data.access_token)
+    return data.access_token
+
+}
+
+async function getGenre (token){
+    const response = await axios('https://api.spotify.com/v1/recommendations/available-genre-seeds', {
+        method: 'GET',
+        headers: {'Authorization': 'Bearer' + token }
+    })
+
+    const data = await response.json()
+    console.log(data.categories.items)
+    return data.categories.items
+}
+
+async function getPlaylistByGenre (token, genreID) {
+    const limit = 10
+
+    const response = await axios(`https://api.spotify.com/v1/browse/categories/${genreID}/playlists?limit=${limit}`,{
+        method: 'GET',
+        headers: {
+            'Authorization' : 'Bearer' + token
+        }
+    })
+
+    const data = await response.json()
+    console.log(data.playlists.items)
+    return data.playlists.items
+}
+
+async function getTracks (token, tracksEndPoint){
+    const limit = 10
+
+    const response = await axios(`${tracksEndPoint}?limit=${limit}`,{
+        method: 'GET',
+        headers: {
+            'Authorization': 'Bearer' + token
+        }
+    })
+
+    const data = await response.json()
+    console.log(data.items)
+    return data.items
+}
+
+async function getTrack (token, trackEndPoint) {
+    const response = await axios(`${trackEndPoint}`, {
+        method: 'GET',
+        headers: {
+            'Authorization': 'Bearer' + token
+        }
+    })
+    
+    const data = await response.json()
+    console.log(data)
+    return data
+}
+
+module.exports = {
+    getToken,
+    getGenre,
+    getPlaylistByGenre,
+    getTracks,
+    getTrack
 }
--- backend/server.js
+++ backend/server.js
@@ -5,16 +5,16 @@ const cors = require('cors')
 
 const app = express()
 
-const PORT = 8080
+const PORT = 3000
 
 const connectDB = require('./config')
 
 connectDB()
 
-const { authorize } = require('./middlemare/authMiddleware')
+const { authorize } = require('./middleware/authMiddleware')
 
 app.use(express.json())
-app.use(cors)
+app.use(cors())
 
 app.use('/', (req,res) => { res.send('Hello World')})
 
